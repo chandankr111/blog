a990e98a1d11332ee83f993e5cc7f3ebce60bd17
chandankr111 <122672735+chandankr111@users.noreply.github.com>
2024-06-29 08:41:31 +0530

    added vercel override

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
 export const BACKEND_URL = "https://backend.chandankr111.workers.dev";
-=======
-export const BACKEND_URL = "https://backend.chandankr111.workers.dev";
->>>>>>> 348da17c4c42a2e830dacad2519097b273315521
+
